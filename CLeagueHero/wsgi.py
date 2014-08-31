b36f7a367447b68bc52c18cf346333bbdab79a0f
Chester O'Neill <c.oneill.codes@gmail.com>
2014-08-31 16:57:32 -0400

    initial commit

---
+++ CLeagueHero/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for CLeagueHero project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CLeagueHero.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
---
+++ arenas/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
+
+from arenas.models import Arena
+
+class IndexView(generic.ListView):
+    template_name = 'arenas/index.html'
+    context_object_name = 'arena_list'
+
+    def get_queryset(self):
+        """Return the last five published polls."""
+        return Arena.objects.order_by('title')
+
+class DetailView(generic.DetailView):
+    model = Arena
+    template_name = 'arenas/detail.html'
