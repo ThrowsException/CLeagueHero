4727d82e7c5957fef2c77a832cfc9c66e8a5b002
Chester ONeill <coneillcodes@gmail.com>
2015-08-15 10:54:19 -0400

    added some style to detail page

--- arenas/forms.py
+++ arenas/forms.py
@@ -3,19 +3,19 @@ from django.forms.extras.widgets import Select
 
 
 class RatingForm(forms.Form):
-    ice_surface = forms.ChoiceField(widget=Select, choices=(
+    ice_surface = forms.ChoiceField(widget=Select(attrs={'class': 'form-control'}), choices=(
         (1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')
     ))
-    referees = forms.ChoiceField(widget=Select, choices=(
+    referees = forms.ChoiceField(widget=Select(attrs={'class': 'form-control'}), choices=(
         (1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')
     ))
-    lockers = forms.ChoiceField(widget=Select, choices=(
+    lockers = forms.ChoiceField(widget=Select(attrs={'class': 'form-control'}), choices=(
         (1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')
     ))
-    league = forms.ChoiceField(widget=Select, choices=(
+    league = forms.ChoiceField(widget=Select(attrs={'class': 'form-control'}), choices=(
         (1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')
     ))
-    teams = forms.ChoiceField(widget=Select, choices=(
+    teams = forms.ChoiceField(widget=Select(attrs={'class': 'form-control'}), choices=(
         (1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')
     ))
-    comment = forms.CharField(widget=forms.Textarea)
+    comment = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
